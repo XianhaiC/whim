d7fe67932e2b1126d2742f5daf3f16aac14a2131
teesung <justinsungg@gmail.com>
2019-08-24 02:07:34 -0700

    Added components for react app. Needs to be tested

---
+++ client/src/components/NewMessageForm.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { API_ROOT, HEADERS } from '../constants';
+
+class NewMessageForm extends React.Component {
+  state = {
+    text: '',
+    impulse_id: this.props.impulse_id
+  };
+
+  componentWillReceiveProps = nextProps => {
+    this.setState({ impulse_id: nextProps.impulse_id });
+  };
+
+  handleChange = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    fetch(`${API_ROOT}/messages`, {
+      method: 'POST',
+      headers: HEADERS,
+      body: JSON.stringify(this.state)
+    });
+    this.setState({ text: '' });
+  };
+
+  render = () => {
+    return (
+      <div className="newMessageForm">
+        <form onSubmit={this.handleSubmit}>
+          <label>New Message:</label>
+          <br />
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  };
+}
+
+export default NewMessageForm;
